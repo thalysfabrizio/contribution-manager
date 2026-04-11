5c94d7188d767b1649289e66e48406c85aec428f
Thalys <thalysfsilva@gmail.com>
2026-04-11 11:45:09 -0300

    chore: add campaign branding migration

---
+++ prisma/migrations/20260411111901_add_campaign_branding/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "Campaign" ADD COLUMN     "accentColor" TEXT,
+ADD COLUMN     "bannerUrl" TEXT,
+ADD COLUMN     "logoUrl" TEXT,
+ADD COLUMN     "messageSignature" TEXT,
+ADD COLUMN     "orgName" TEXT;
