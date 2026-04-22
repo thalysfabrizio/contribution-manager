ab5148ddc36f59ae8e97f9a2fe4e887974b1d984
Thalys <thalysfsilva@gmail.com>
2026-04-22 19:27:46 -0300

    chore: log slow Prisma queries above 100ms

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,26 @@
 import { PrismaClient } from '@/generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { env } from './env';
+import { logger } from './logger';
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+const SLOW_QUERY_MS = 100;
+
 function createPrismaClient() {
   const adapter = new PrismaPg(env.DATABASE_URL);
-  return new PrismaClient({ adapter });
+  const client = new PrismaClient({
+    adapter,
+    log: [{ level: 'query', emit: 'event' }],
+  });
+  client.$on('query', (e) => {
+    if (e.duration >= SLOW_QUERY_MS) {
+      logger.warn('Slow Prisma query', { duration: e.duration, query: e.query });
+    }
+  });
+  return client;
 }
 
 export const prisma = globalForPrisma.prisma ?? createPrismaClient();
